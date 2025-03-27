200c8948022752c0861c749fe49224a9c883a77b
Lemin737333 <117698302+Lemin737333@users.noreply.github.com>
2025-03-27 11:23:45 -0700

    test

--- mods/Dinos.js
+++ mods/Dinos.js
@@ -14,7 +14,7 @@ elements.dinos2M1Description = {
     alert(info1stMod)
     return
   },
-};
+},
 
 elements.raptor= {
     color: ["#ea4335","#ff6d01","#fbbc04","#34a853","#46bdc6","#4285f4","#a835ea"],
@@ -118,136 +118,136 @@ elements.raptor= {
     // stateLow: "raptor_skeleton",
    cooldown: defaultCooldown,
 	baby:"raptor"
-};
+},
 
 
-elements.t_rex= {
-    color: ["#ea4335","#ff6d01","#fbbc04","#34a853","#46bdc6","#4285f4","#a835ea"],
-    behavior: [
-		"XX|XX|XX",
-		"XX|XX|XX",
-		"XX|XX|XX"
-	 ],
-	    properties: {
-			 //visual Genetics.
-			 color1: "",
-			 color2:"",
-			 hight:1,
-			 length:1,
-			 pattern:4,
-		 		/*
-			 1:skull
-			 2:boots
-			 3:back cresting
-			 4:polka dots
-			 */
-			 patternV:1,
-			 patternM1:[],
-			 patternM2:[],
-			 //physical genetics.
-			 speed:1,
-			 jump:1,
-			 temper1:30,
-			 temper2:10,
-			 //movement
-			tcool:0,
-         dir: 1,
-			frame:0,
-			 yv:0,
-			 jaw:0
-    },
-	onPlace: function(pixel){
-		let color = Math.floor(Math.random(0,2)*7);
-		pixel.color2 = ["#ea4335","#ff6d01","#fbbc04","#34a853","#46bdc6","#4285f4","#a835ea"][color]		
-		color = Math.floor(Math.random(0,2)*7);
-		pixel.color1 = ["#ea4335","#ff6d01","#fbbc04","#34a853","#46bdc6","#4285f4","#a835ea"][color]		
-		// pixel.color1 = ["#d4d4d4","#8f8f8f","#616161",pixel.color2][color]		
-		pixel.temper1 = Math.floor(Math.random(0,2)*20)+10;
-		pixel.temper2 = Gtemper = Math.floor(Math.random(0,2)*20)+10;
-		pixel.speed = Math.floor(Math.random(0,2)*2)+1;
-		pixel.jump = Math.floor(Math.random(0,2)*3)+1;
-		pixel.hight = Math.floor(Math.random(0,2)*2);
-		pixel.length = Math.floor(Math.random(0,2)*2)+1;
-		// pixel.pattern = Math.floor(Math.random(0,2)*5)+1;
-		pixel.patternV = Math.floor(Math.random(0,2)*3)-1;
-		if(pixel.pattern === 3){
-													    for (var i = 0; i < 4; i++) {
-			pixel.patternM1.push(Math.floor(Math.random(0,2)*3)+1)
-			}
-			}
-				if(pixel.pattern === 4){
-					let a = 0;
-					let b = 0;
+// elements.t_rex= {
+//     color: ["#ea4335","#ff6d01","#fbbc04","#34a853","#46bdc6","#4285f4","#a835ea"],
+//     behavior: [
+// 		"XX|XX|XX",
+// 		"XX|XX|XX",
+// 		"XX|XX|XX"
+// 	 ],
+// 	    properties: {
+// 			 //visual Genetics.
+// 			 color1: "",
+// 			 color2:"",
+// 			 hight:1,
+// 			 length:1,
+// 			 pattern:4,
+// 		 		/*
+// 			 1:skull
+// 			 2:boots
+// 			 3:back cresting
+// 			 4:polka dots
+// 			 */
+// 			 patternV:1,
+// 			 patternM1:[],
+// 			 patternM2:[],
+// 			 //physical genetics.
+// 			 speed:1,
+// 			 jump:1,
+// 			 temper1:30,
+// 			 temper2:10,
+// 			 //movement
+// 			tcool:0,
+//          dir: 1,
+// 			frame:0,
+// 			 yv:0,
+// 			 jaw:0
+//     },
+// 	onPlace: function(pixel){
+// 		let color = Math.floor(Math.random(0,2)*7);
+// 		pixel.color2 = ["#ea4335","#ff6d01","#fbbc04","#34a853","#46bdc6","#4285f4","#a835ea"][color]		
+// 		color = Math.floor(Math.random(0,2)*7);
+// 		pixel.color1 = ["#ea4335","#ff6d01","#fbbc04","#34a853","#46bdc6","#4285f4","#a835ea"][color]		
+// 		// pixel.color1 = ["#d4d4d4","#8f8f8f","#616161",pixel.color2][color]		
+// 		pixel.temper1 = Math.floor(Math.random(0,2)*20)+10;
+// 		pixel.temper2 = Gtemper = Math.floor(Math.random(0,2)*20)+10;
+// 		pixel.speed = Math.floor(Math.random(0,2)*2)+1;
+// 		pixel.jump = Math.floor(Math.random(0,2)*3)+1;
+// 		pixel.hight = Math.floor(Math.random(0,2)*2);
+// 		pixel.length = Math.floor(Math.random(0,2)*2)+1;
+// 		// pixel.pattern = Math.floor(Math.random(0,2)*5)+1;
+// 		pixel.patternV = Math.floor(Math.random(0,2)*3)-1;
+// 		if(pixel.pattern === 3){
 // 													    for (var i = 0; i < 4; i++) {
-// a = Math.floor(Math.random(0,2)*2)+1
-// b = Math.floor(Math.random(0,2)*2)-1
-// pixel.patternM1.push([
-// 	b,a,i
-// 	])
+// 			pixel.patternM1.push(Math.floor(Math.random(0,2)*3)+1)
 // 			}
-}
-		},
-	renderer: function(pixel,ctx) {
-		      let rgb = hexToRGB(pixel.color1);
-            let temp1= "rgb("+(rgb.r-20)+","+(rgb.g-20)+","+(rgb.b-20)+")";
-				rgb = hexToRGB(pixel.color2);
-            let temp2 = "rgb("+(rgb.r-20)+","+(rgb.g-20)+","+(rgb.b-20)+")";
-		let colors = [pixel.color1,temp1,pixel.color2,temp2]; 
-		//body
-		drawRect(ctx,colors[0],pixel.x+(pixel.dir == 1? -2:-1),pixel.y+2,4,2)
-		drawRect(ctx,colors[(pixel.pattern ==1? 2:0)],pixel.x+(pixel.dir == 1? -1:-2),pixel.y+1,4,1)
-		drawRect(ctx,colors[1],pixel.x+(pixel.dir == 1? 1:-1),pixel.y+2,1,2)
-		//jaw
-				drawRect(ctx,colors[(pixel.pattern ==1? 2:0)],pixel.x+(pixel.dir == 1? 1:-2),pixel.y-1-Math.floor(pixel.jaw*3),2,2)
-				drawRect(ctx,colors[(pixel.pattern ==1? 2:0)],pixel.x+(pixel.dir == 1? 0:-1),pixel.y-1-Math.floor(pixel.jaw*2),2,2)
-				drawRect(ctx,colors[(pixel.pattern ==1? 2:0)],pixel.x+(pixel.dir == 1? -1:0),pixel.y-1-Math.floor(pixel.jaw*1),2,2)
-		//legs
-		drawRect(ctx,colors[(pixel.pattern ==2? 3:1)],pixel.x+(pixel.dir == 1? 1:-1),pixel.y+4,1,1,(Math.floor(pixel.frame)!==1?1:0))
-		drawRect(ctx,colors[(pixel.pattern ==2? 2:0)],pixel.x+(pixel.dir == 1? -1:1),pixel.y+4,1,1,(Math.floor(pixel.frame)!==3?1:0))
-		//tail
-		if(pixel.pattern==2) drawRect(ctx,colors[2],pixel.x+pixel.patternV*pixel.dir,pixel.y+2,1,1)
-		drawRect(ctx,colors[(pixel.pattern ==2? 2:0)],pixel.x+(pixel.dir == 1? -3:-1),pixel.y+3,5,1)
-				if(pixel.pattern==3) for (var i = 0; i < pixel.patternM1.length; i++) {
-			drawRect(ctx,colors[2],pixel.x+(pixel.dir == 1? (Math.min(i-2,0)):(Math.max(3-i,1)-pixel.patternM1[i][0]))-pixel.dir,pixel.y+3-i,pixel.patternM1[i][0],1)
-							 }
-						if(pixel.pattern==4) for (var i = 0; i < pixel.patternM1.length; i++) {
-			drawRect(ctx,colors[2],pixel.x+(pixel.dir == 1? (pixel.patternM1[i][0]-pixel.patternM1[i][1]):(1-pixel.patternM1[i][0]))-pixel.dir,pixel.y+2-pixel.patternM1[i][2],pixel.patternM1[i][1],pixel.patternM1[i][1])
-							 }
-		drawRect(ctx,"#1b1a24",pixel.x,pixel.y-Math.floor(pixel.jaw*2),1,1)
-},
-			tick: function(pixel) {
-				let bottom = 4
-				let left = (pixel.dir == 1? 1:-1)
-				let right = (pixel.dir == 1? -1:1)
-				pixel.tcool=pixel.tcool-1;
-				pixel.yv=Math.max(pixel.yv-1,0);
-				pixel.jaw=Math.abs(Math.sin(pixelTicks/2.5));
-					//move & turn
-				if(pixel.temper2/100>=Math.random()){
-					if(isSolid(pixel.x+left,pixel.y+(bottom+1)) || isSolid(pixel.x+right,pixel.y+(bottom+1))) pixel.yv=pixel.jump
-					}
-				if(pixel.frame!==0)pixel.frame=((pixel.frame+0.25)%4);
-				if(Math.random()>0.5) {
-					if(!isSolid(pixel.x+left+(pixel.dir*pixel.speed),pixel.y+(bottom)) && !isSolid(pixel.x+right+(pixel.dir*pixel.speed),pixel.y+(bottom))) tryMove(pixel,pixel.x+(pixel.dir*pixel.speed),pixel.y);
-					if(pixel.frame==0)pixel.frame=0.25
-					if(isSolid(pixel.x+left,pixel.y+(bottom-1)) || isSolid(pixel.x+right,pixel.y+(bottom-1))) pixel.yv=pixel.jump
-				}
-				if(Math.random()>0.8 && 1>pixel.tcool) {pixel.dir = pixel.dir*-1;pixel.tcool=pixel.temper1};
-				//fall
-				tryMove(pixel,pixel.x,pixel.y-pixel.yv);
-				if(!isSolid(pixel.x+left,pixel.y+(bottom+1)) && !isSolid(pixel.x+right,pixel.y+(bottom+1))) tryMove(pixel,pixel.x,pixel.y+1);
-},
-	category: "ancient life",
-	maxSize: 1,
-    state: "solid",
-	// breakInto:"raptor_skeleton",
-	    // tempHigh: 1700,
-    // stateHigh: "raptor_skeleton",
-	    // tempLow: -200,
-    // stateLow: "raptor_skeleton",
-   cooldown: defaultCooldown,
-	baby:"t_rex"
-};
+// 			}
+// 				if(pixel.pattern === 4){
+// 					let a = 0;
+// 					let b = 0;
+// // 													    for (var i = 0; i < 4; i++) {
+// // a = Math.floor(Math.random(0,2)*2)+1
+// // b = Math.floor(Math.random(0,2)*2)-1
+// // pixel.patternM1.push([
+// // 	b,a,i
+// // 	])
+// // 			}
+// }
+// 		},
+// 	renderer: function(pixel,ctx) {
+// 		      let rgb = hexToRGB(pixel.color1);
+//             let temp1= "rgb("+(rgb.r-20)+","+(rgb.g-20)+","+(rgb.b-20)+")";
+// 				rgb = hexToRGB(pixel.color2);
+//             let temp2 = "rgb("+(rgb.r-20)+","+(rgb.g-20)+","+(rgb.b-20)+")";
+// 		let colors = [pixel.color1,temp1,pixel.color2,temp2]; 
+// 		//body
+// 		drawRect(ctx,colors[0],pixel.x+(pixel.dir == 1? -2:-1),pixel.y+2,4,2)
+// 		drawRect(ctx,colors[(pixel.pattern ==1? 2:0)],pixel.x+(pixel.dir == 1? -1:-2),pixel.y+1,4,1)
+// 		drawRect(ctx,colors[1],pixel.x+(pixel.dir == 1? 1:-1),pixel.y+2,1,2)
+// 		//jaw
+// 				drawRect(ctx,colors[(pixel.pattern ==1? 2:0)],pixel.x+(pixel.dir == 1? 1:-2),pixel.y-1-Math.floor(pixel.jaw*3),2,2)
+// 				drawRect(ctx,colors[(pixel.pattern ==1? 2:0)],pixel.x+(pixel.dir == 1? 0:-1),pixel.y-1-Math.floor(pixel.jaw*2),2,2)
+// 				drawRect(ctx,colors[(pixel.pattern ==1? 2:0)],pixel.x+(pixel.dir == 1? -1:0),pixel.y-1-Math.floor(pixel.jaw*1),2,2)
+// 		//legs
+// 		drawRect(ctx,colors[(pixel.pattern ==2? 3:1)],pixel.x+(pixel.dir == 1? 1:-1),pixel.y+4,1,1,(Math.floor(pixel.frame)!==1?1:0))
+// 		drawRect(ctx,colors[(pixel.pattern ==2? 2:0)],pixel.x+(pixel.dir == 1? -1:1),pixel.y+4,1,1,(Math.floor(pixel.frame)!==3?1:0))
+// 		//tail
+// 		if(pixel.pattern==2) drawRect(ctx,colors[2],pixel.x+pixel.patternV*pixel.dir,pixel.y+2,1,1)
+// 		drawRect(ctx,colors[(pixel.pattern ==2? 2:0)],pixel.x+(pixel.dir == 1? -3:-1),pixel.y+3,5,1)
+// 				if(pixel.pattern==3) for (var i = 0; i < pixel.patternM1.length; i++) {
+// 			drawRect(ctx,colors[2],pixel.x+(pixel.dir == 1? (Math.min(i-2,0)):(Math.max(3-i,1)-pixel.patternM1[i][0]))-pixel.dir,pixel.y+3-i,pixel.patternM1[i][0],1)
+// 							 }
+// 						if(pixel.pattern==4) for (var i = 0; i < pixel.patternM1.length; i++) {
+// 			drawRect(ctx,colors[2],pixel.x+(pixel.dir == 1? (pixel.patternM1[i][0]-pixel.patternM1[i][1]):(1-pixel.patternM1[i][0]))-pixel.dir,pixel.y+2-pixel.patternM1[i][2],pixel.patternM1[i][1],pixel.patternM1[i][1])
+// 							 }
+// 		drawRect(ctx,"#1b1a24",pixel.x,pixel.y-Math.floor(pixel.jaw*2),1,1)
+// },
+// 			tick: function(pixel) {
+// 				let bottom = 4
+// 				let left = (pixel.dir == 1? 1:-1)
+// 				let right = (pixel.dir == 1? -1:1)
+// 				pixel.tcool=pixel.tcool-1;
+// 				pixel.yv=Math.max(pixel.yv-1,0);
+// 				pixel.jaw=Math.abs(Math.sin(pixelTicks/2.5));
+// 					//move & turn
+// 				if(pixel.temper2/100>=Math.random()){
+// 					if(isSolid(pixel.x+left,pixel.y+(bottom+1)) || isSolid(pixel.x+right,pixel.y+(bottom+1))) pixel.yv=pixel.jump
+// 					}
+// 				if(pixel.frame!==0)pixel.frame=((pixel.frame+0.25)%4);
+// 				if(Math.random()>0.5) {
+// 					if(!isSolid(pixel.x+left+(pixel.dir*pixel.speed),pixel.y+(bottom)) && !isSolid(pixel.x+right+(pixel.dir*pixel.speed),pixel.y+(bottom))) tryMove(pixel,pixel.x+(pixel.dir*pixel.speed),pixel.y);
+// 					if(pixel.frame==0)pixel.frame=0.25
+// 					if(isSolid(pixel.x+left,pixel.y+(bottom-1)) || isSolid(pixel.x+right,pixel.y+(bottom-1))) pixel.yv=pixel.jump
+// 				}
+// 				if(Math.random()>0.8 && 1>pixel.tcool) {pixel.dir = pixel.dir*-1;pixel.tcool=pixel.temper1};
+// 				//fall
+// 				tryMove(pixel,pixel.x,pixel.y-pixel.yv);
+// 				if(!isSolid(pixel.x+left,pixel.y+(bottom+1)) && !isSolid(pixel.x+right,pixel.y+(bottom+1))) tryMove(pixel,pixel.x,pixel.y+1);
+// },
+// 	category: "ancient life",
+// 	maxSize: 1,
+//     state: "solid",
+// 	// breakInto:"raptor_skeleton",
+// 	    // tempHigh: 1700,
+//     // stateHigh: "raptor_skeleton",
+// 	    // tempLow: -200,
+//     // stateLow: "raptor_skeleton",
+//    cooldown: defaultCooldown,
+// 	baby:"t_rex"
+// };
 
 
 elements.bronto= {
